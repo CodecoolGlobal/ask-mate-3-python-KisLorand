156883961753a67af9be50b3fef971813f743f7a
cserdniel <cserdniel@hotmail.com>
2022-01-31 16:28:27 +0100

    Mega change by Lóránd and Dani

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
 import flask
-<<<<<<< HEAD
 from flask import Flask
 import data_manager
-=======
-from flask import Flask, request, redirect
->>>>>>> dab6a9b49afdc1c2b34e230cd8671944f514a7b9
+
 
 app = Flask(__name__)
 
@@ -21,26 +18,26 @@ def list_all():
 
 @app.route("/add-question", methods=['GET', 'POST'])
 def add_question():
-    if request.method == "POST":
-        title = request.form['title']
-        message = request.form['message']
+    if flask.request.method == "POST":
+        title = flask.request.form['title']
+        message = flask.request.form['message']
 
-        return redirect('/')
+        return flask.redirect('/')
     return flask.render_template("add_question.html")
 
 
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 def open_question(question_id):
     pass
-    return flask.render_template()
+    return flask.render_template("useless_main_page.html")
 
 
 @app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
 def new_answer(question_id):
     if flask.request.method == "POST":
-        data_handler.add_new_answer(question_id, flask.request.form.get("message"))
+        data_manager.add_new_answer(question_id, flask.request.form.get("message"))
         return flask.redirect('/question/<question_id>')
-    return flask.render_template("new_answer.html")
+    return flask.render_template("add_answer.html")
 
 
 if __name__ == "__main__":
