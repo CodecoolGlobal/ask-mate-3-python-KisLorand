5f987273877262c7d264164d2e5294c1a39298e1
KisLorand <kislorand270@gmail.com>
2022-01-31 15:18:16 +0100

    merge

--- server.py
+++ server.py
@@ -4,35 +4,6 @@ from flask import Flask, request, redirect
 app = Flask(__name__)
 
 
-@app.route("/")
-def main_page():
-    return flask.render_template("useless_main_page.html")
-
-
-@app.route('/list')
-def list_all():
-    return flask.render_template("useless_main_page.html")
-
-
-@app.route("/add-question", methods=['GET', 'POST'])
-def add_question():
-    if request.method == "POST":
-        title = request.form['title']
-        message = request.form['message']
-        
-        return redirect('/')
-    return flask.render_template("add_question.html")
-
-
-@app.route('/question/<question_id>', methods=['GET', 'POST'])
-def open_question(quiestion_id):
-    pass
-    return flask.render_template()
-
-
-@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
-def new_answer(question_id):
-    return "new_answer"
 
 
 
