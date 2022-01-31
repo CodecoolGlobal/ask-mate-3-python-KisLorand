d5c79464189530e6f0799375de8ba37f8533d610
cserdniel <cserdniel@hotmail.com>
2022-01-31 14:18:41 +0100

    Add skeleton of new_answer()

--- server.py
+++ server.py
@@ -8,5 +8,10 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
+def new_answer(question_id):
+    return "new_answer"
+
+
 if __name__ == "__main__":
     app.run()
