0f95c52102f45cef9dc4c88105c70f69c87922f2
cserdniel <cserdniel@hotmail.com>
2022-02-01 10:55:38 +0100

    Add import time, and UNIX timestamp to add_new_answer()

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,10 @@
 import connection
+import time
 
 
 def add_new_answer(id_input, input_text):
     all_answers = connection.get_all_csv_data("/sample_data/answer.csv")
-    new_answer = {"id": "0", "submission_time": "0", "vote_number": "1", "question_id": id_input,
+    new_answer = {"id": "0", "submission_time": time.time(), "vote_number": "1", "question_id": id_input,
                   "message": input_text, "image": ""}
     all_answers.append(new_answer)
 
