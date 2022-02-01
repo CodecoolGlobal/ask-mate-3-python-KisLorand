0eed92a28561a86d108fafbbd7ba2dfdbd18ae84
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-02-01 10:41:41 +0100

    index.html modified

--- data_manager.py
+++ data_manager.py
@@ -4,5 +4,6 @@ def add_new_answer(id_input, input_text):
     new_answer = {}
 
 
-def get_all_data(path):
-    return connection.get_all_csv_data(path)
+def get_all_data():
+    return connection.get_all_csv_data()
+
