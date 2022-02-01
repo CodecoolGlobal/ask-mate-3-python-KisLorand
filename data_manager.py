09b9932bb2a884f5745e927828043ee2d1395c99
cserdniel <cserdniel@hotmail.com>
2022-02-01 11:52:04 +0100

    Add all answers export to add_new_answer()

--- data_manager.py
+++ data_manager.py
@@ -5,11 +5,11 @@ PATH_ANSWERS = "sample_data/answer.csv"
 PATH_QUESTIONS = "sample_data/question.csv"
 
 def add_new_answer(id_input, input_text):
-    all_answers = connection.get_all_csv_data(path="sample_data/answer.csv")
+    all_answers = connection.get_all_csv_data(PATH_ANSWERS)
     new_answer = {"id": "0", "submission_time": time.time(), "vote_number": "1", "question_id": id_input,
                   "message": input_text, "image": ""}
     all_answers.append(new_answer)
-    print(new_answer)
+    connection.write_all_data_to_csv(PATH_ANSWERS, all_answers)
 
 def get_all_data(type):
     if type.upper() == "ANSWERS":
