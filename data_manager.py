0907ddf1fe2350c6e12125f2ef751594613ecbf3
KisLorand <kislorand270@gmail.com>
2022-03-03 11:20:52 +0100

    change querys in update_table_single_col, update_table_single_col, get_all_data, get_all_data_by_condition, question_opener, add_new_question

--- data_manager.py
+++ data_manager.py
@@ -8,6 +8,7 @@ import datetime
 from sql_connection import connection_handler
 import bcrypt
 
+from psycopg2.sql import SQL, Literal, Identifier
 
 # image path
 UPLOAD_FOLDER = 'static/images/'
@@ -22,7 +23,8 @@ MIN_QUESTION_MESSAGE_LEN = 5
 
 @connection_handler
 def update_table_single_col(cursor, table_name, col_name, id_number, vote_up):
-    query = f""" UPDATE {table_name} SET {col_name}={col_name}+{vote_up} WHERE id={id_number} """
+    # query = f""" UPDATE {table_name} SET {col_name}={col_name}+{vote_up} WHERE id={id_number} """
+    query = SQL('UPDATE {} SET {}={}+{} WHERE id={}').format(Identifier(table_name), Identifier(col_name), Identifier(col_name), Literal(vote_up), Literal(id_number) )
     cursor.execute(query)
 
 
@@ -30,22 +32,26 @@ def update_table_single_col(cursor, table_name, col_name, id_number, vote_up):
 def add_new_answer(cursor, id_input, input_text, image_file, user_name):
     user_id = search_user_id(user_name)
     current_time: datetime = datetime.datetime.now()
-    query = f""" INSERT INTO answer (submission_time, vote_number, question_id, message, user_id) 
-                VALUES ('{current_time}', 0, '{id_input}', '{input_text}', {user_id}) """
+    # query = f""" INSERT INTO answer (submission_time, vote_number, question_id, message, user_id)
+    #             VALUES ('{current_time}', 0, '{id_input}', '{input_text}', {user_id}) """
+    query = SQL('INSERT INTO answer (submission_time, vote_number, question_id, message, user_id) VALUES ({}, {}, {}, {}, {})')\
+        .format(Literal(current_time), Literal(0), Literal(id_input), Literal(input_text), Literal(user_id))
     cursor.execute(query)
     select_query = f""" SELECT id FROM answer ORDER BY id DESC LIMIT 1 """
     cursor.execute(select_query)
 
     image_index = cursor.fetchone().get('id')
     image_path = upload_image(f"A_{image_index}", image_file)
-    new_answer_query = f""" UPDATE answer SET image='{image_path}' WHERE id='{image_index}' """
+    # new_answer_query = f""" UPDATE answer SET image='{image_path}' WHERE id='{image_index}' """
+    new_answer_query = SQL(' UPDATE answer SET image={} WHERE id={} ').format(Literal(image_path), Literal(image_index))
     cursor.execute(new_answer_query)
     return True
 
 
 @connection_handler
-def get_all_data(cursor, table_name, order_type="submission_time", order_direction="DESC"):
-    query = f""" SELECT * FROM "{table_name}" ORDER BY {order_type} {order_direction} """
+def get_all_data(cursor, table_name, order_type='submission_time', order_direction='DESC'):
+    # query = f""" SELECT * FROM "{table_name}" ORDER BY {order_type} {order_direction} """
+    query = SQL(' SELECT * FROM {} ORDER BY {} DESC ').format(Identifier(table_name), Identifier(order_type)) #, Literal(order_direction))
     cursor.execute(query)
     table_data = cursor.fetchall()
     return table_data
@@ -53,7 +59,9 @@ def get_all_data(cursor, table_name, order_type="submission_time", order_directi
 
 @connection_handler
 def get_all_data_by_condition(cursor, table_name, column, col_value, order_type="submission_time", order_direction="ASC"):
-    query = f""" SELECT * FROM "{table_name}" WHERE {column}>={col_value} ORDER BY {order_type} {order_direction} """
+    # query = f""" SELECT * FROM "{table_name}" WHERE {column}>={col_value} ORDER BY {order_type} {order_direction} """
+    query = SQL(' SELECT * FROM {} WHERE {}>={} ORDER BY {} ASC ')\
+        .format(Identifier(table_name), Identifier(column), Literal(col_value), Identifier(order_type)) #, Literal(order_direction))
     cursor.execute(query)
     table_data = cursor.fetchall()
     return table_data
@@ -61,12 +69,14 @@ def get_all_data_by_condition(cursor, table_name, column, col_value, order_type=
 
 @connection_handler
 def question_opener(cursor, question_id):
-    question_query = f""" SELECT * FROM question WHERE id='{question_id}' """
-    cursor.execute(question_query)
+    # question_query = f""" SELECT * FROM question WHERE id='{question_id}' """
+    question_query = SQL(' SELECT * FROM question WHERE id=%s ')
+    cursor.execute(question_query, [question_id])
     question_data = cursor.fetchone()
 
-    answer_query = f""" SELECT * FROM answer WHERE question_id = '{question_id}' """
-    cursor.execute(answer_query)
+    # answer_query = f""" SELECT * FROM answer WHERE question_id = '{question_id}' """
+    answer_query = SQL(' SELECT * FROM answer WHERE question_id = %s ')
+    cursor.execute(answer_query, [question_id])
     answers = cursor.fetchall()
 
     return question_data, answers
@@ -112,16 +122,20 @@ def add_new_question(cursor, new_title, new_message, image_file, user_name):
     if is_new_question_valid(new_title, new_message):
         user_id = search_user_id(user_name)
         current_time = datetime.datetime.now()
-        insert_new_question = f""" INSERT INTO question (submission_time, view_number, vote_number, title, message, user_id) 
-                                   VALUES ('{current_time}', 0, 0, '{new_title}', '{new_message}', {user_id}) """
+        # insert_new_question = f""" INSERT INTO question (submission_time, view_number, vote_number, title, message, user_id)
+        #                            VALUES ('{current_time}', 0, 0, '{new_title}', '{new_message}', {user_id}) """
+        values = [current_time, 0, 0, new_title, new_message, user_id]
+        insert_new_question =SQL(' INSERT INTO question (submission_time, view_number, vote_number, title, message, user_id) '
+                                 'VALUES  {inserted_values}').format( inserted_values=SQL(', ').join([Identifier(value) for value in values]) )
         cursor.execute(insert_new_question)
         select_query = f""" SELECT id FROM question ORDER BY id DESC LIMIT 1 """
         cursor.execute(select_query)
 
         image_index = cursor.fetchone().get('id')
         image_path = upload_image(f"Q_{image_index}", image_file)
-        new_answer_query = f""" UPDATE question SET image='{image_path}' WHERE id='{image_index}' """
-        cursor.execute(new_answer_query)
+        # new_answer_query = f""" UPDATE question SET image='{image_path}' WHERE id='{image_index}' """
+        new_answer_query = SQL(' UPDATE question SET image=%s WHERE id=%s ')
+        cursor.execute(new_answer_query, [image_path, image_index])
         return True
 
 
@@ -363,9 +377,6 @@ def get_question_tag_by_id(cursor, question_id):
     WHERE question_id='{question_id}'
     """
     cursor.execute(query)
-<<<<<<< HEAD
-
-=======
     return cursor.fetchall()
 
 
@@ -377,4 +388,3 @@ def get_all_tags(cursor):
     """
     cursor.execute(query)
     return cursor.fetchall()
->>>>>>> 8a160f67b32d5a1483017858f9db7f648cacad09
